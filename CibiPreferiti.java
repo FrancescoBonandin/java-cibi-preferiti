d3dfd97be59b81b5cf95153b4c1003ca8a89b6c8
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-11-09 20:34:37 +0100

    Bonus completato

--- CibiPreferiti.java
+++ CibiPreferiti.java
@@ -1,5 +1,8 @@
 package org.lessons.java.array;
 
+import java.util.Arrays;
+import java.util.Random;
+
 // Todo
 //Nel progetto` java-cibi-preferiti` aggiungete la classe `CibiPreferiti` nel package `org.lessons.java.array` col suo metodo `main`.
 //Nel programma inizializzate un array (con i metodi visti a lezione) con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi).
@@ -13,12 +16,47 @@ public class CibiPreferiti {
 
 	public static void main(String[] args) {
 		
-		String[] favouriteFood = {"pizza", "pasta", "sushi", "patatine", "pane"} ;
+		String[] favouriteFood1 = {"pizza", "pasta", "sushi", "patatine", "pane"} ;
 		
-		System.out.println(favouriteFood.length);
-		System.out.println(favouriteFood[0]);
-		System.out.println(favouriteFood[favouriteFood.length-1]);
+		System.out.println("lunghezza classifica:" + favouriteFood1.length);
+		System.out.println("Top of the top foods: " + favouriteFood1[0]);
+		System.out.println("Bottom of the top foods: " + favouriteFood1[favouriteFood1.length-1]);
+		
+		
+		
+		System.out.println("\n ");
 
+		//--------BONUS-----------
+		System.err.println("**BONUS**");
+		String [] favouriteFood2 = {"pizza", "pasta", "sushi", "patatine", "pane", "arrosto"} ;
+		
+		Random rnd = new Random() ;
+		boolean randBoolValue=rnd.nextBoolean();
+		
+		String[] chosenArray = randBoolValue ? favouriteFood1 : favouriteFood2;		
+		
+		System.out.println("l'array scelto ha lunghezza: " + chosenArray.length + " ed è composto da questi elementi" + Arrays.toString(chosenArray));
+			
+		if(chosenArray.length %2 == 0) {
+			
+			 System.out.println("The middle elements are: ");
+		         
+	         //	Pari
+	         String leftMediumValue = chosenArray[(chosenArray.length/2) - 1];
+	         System.out.println(leftMediumValue);
+	         String rightMediumValue = chosenArray[chosenArray.length/2];
+	         System.out.println(rightMediumValue);
+		}
+		
+		else {
+			//	Dispari	
+			
+			System.out.println("The middle element is: ");
+			String mediumValue = chosenArray[chosenArray.length/2];
+			System.out.println(mediumValue);
+			
+		}
+		
 	}
 
 }
