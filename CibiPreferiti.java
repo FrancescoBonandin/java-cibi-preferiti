9f61b83d3ed8d453649067df045ff7144cfcf58f
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-11-09 17:47:15 +0100

    first commit

---
+++ CibiPreferiti.java
@@ -0,0 +1,19 @@
+package org.lessons.java.array;
+
+// Todo
+//Nel progetto` java-cibi-preferiti` aggiungete la classe `CibiPreferiti` nel package `org.lessons.java.array` col suo metodo `main`.
+//Nel programma inizializzate un array (con i metodi visti a lezione) con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi).
+//
+//Stampate poi a video:
+//- la lunghezza della classifica
+//- il vostro cibo top (prima posizione della classifica)
+//- il vostro cibo preferito ma non troppo (ultima posizione della classifica)
+
+public class CibiPreferiti {
+
+	public static void main(String[] args) {
+		
+		String[] favoriteFood = {"pizza", "pasta", "sushi", "patatine", "pane"} ;
+	}
+
+}
